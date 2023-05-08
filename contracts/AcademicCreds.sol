68640666588ee14de7df0b061df1379dfdf6147d
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-05-07 20:33:10 -0500

    WIP: AcademicCreds.sol

--- contracts/AcademicCreds.sol
+++ contracts/AcademicCreds.sol
@@ -3,11 +3,27 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+// import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
-contract AcademicCreds is ERC1155, Ownable, ERC1155Burnable {
-    constructor() ERC1155("") {}
+contract AcademicCreds is ERC1155, Ownable {
 
+    constructor() ERC1155("") {
+        //   ERC1155(uri_)  ... the uri is for a "base" URI for the metadata.
+        //                      we won't have a URI since using IPFS hash for
+        //                      each individual NFT token
+        //                      We may need to put the IPFS node URI here!!??
+
+
+        //  Our constructor should also set up the 2 token ID's:
+        //      Transcript Tokens
+        //      Diploma Tokens
+
+        // Code goes here...
+
+    }
+
+    // allows owner to update the base URI where metadata is located
+    // this will only be needed if the base URI is an IPFS node that could change
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
@@ -19,10 +35,127 @@ contract AcademicCreds is ERC1155, Ownable, ERC1155Burnable {
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
+    // I don't *think* we need mintBatch .. a school is issuing 1 credential at a time?
+    //
+    // function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    //     public
+    //     onlyOwner
+    // {
+    //     _mintBatch(to, ids, amounts, data);
+    // }
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    // OVER-RIDE STANDARD ERC1155 FUNCTIONS FOR SOULBOUND PROPERTIES
+    //
+
+    function setApprovalForAll(address operator, bool approved)
+        external
     {
-        _mintBatch(to, ids, amounts, data);
+        // might rewrite this to approve for VIEWING the tokens only??
+        // otherwise, should just fail with error
+
+        // if operator != msg.sender
+        //      fail here ...
+        // else
+        //      it will fail in the openZeppelin parent
+
     }
+
+    function isApprovedForAll(address account, address operator)
+        external
+        returns(bool)
+    {
+        // if we use approvals for VIEWING the tokens, this will return true
+
+        // if we don't use approvals at all, this should just return false
+
+        return false;
+    }
+
+    function safeTransferFrom
+        (
+            address from,
+            address to,
+            uint256 id,
+            uint256 amount,
+            bytes data
+        )
+        external
+    {
+
+        // Transfer functions should fail; 'token is not transferrable'
+        // if 'to' != '0x0' then
+        //      fail here ...
+        // else
+        //      it will fail in the openZeppelin parent
+
+    }
+
+
+    function safeBatchTransferFrom
+        (
+            address from,
+            address to,
+            uint256[] ids,
+            uint256[] amounts,
+            bytes data
+        )
+        external
+    {
+        // Transfer functions should fail; 'token is not transferrable'
+        // if 'to' != '0x0' then
+        //      fail here ...
+        // else
+        //      it will fail in the openZeppelin parent
+
+    }
+
+
+
+
+    // Soulbound burns:
+    // --------------------------------------------------------------------
+    // Would like to allow a token holder to "burn" their academic tokens.
+    //
+    // Openzeppelin has an ERC1155 extension for burnable tokens, but it
+    // allows "approved" accounts to burn tokens in other accounts.
+    //
+    // Since our Soulbound version does not utilize Approvals in this way,
+    // it is best to create our own burn functions that enforces only
+    // burning one's own tokens.
+
+
+    function burn(address from, uint256 id, uint256 amount)
+        public
+    {
+
+        //      Code goes here ...
+
+
+        // Enforced by the internal _burn() from openZeppelin:
+        //      'from' cannot be the zero address
+        //      'from' must have at least 'amount' tokens of type 'id'
+        //      emits a TransferSingle event
+        _burn(from, id, amount);
+    }
+
+    function burnBatch(address from, uint256[] ids, uint256[] amounts)
+        public
+    {
+        // batched version of burn
+
+        //      Code goes here...
+
+
+        // Enforced by the internal _burnBatch:
+        //      'from' cannot be the zero address
+        //      'ids' and 'amounts' must have the same length
+        //      'from' must have at least the amount of each type listed
+        _burnBatch(from, ids, amounts);
+    }
+
 }
