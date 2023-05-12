c6a4e73fee88b18878b496a5d774d00f4ab2a08b
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-05-12 18:10:49 -0500

    adds redux; load provider/chainId/account to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,39 @@
 import logo from '../logo.svg';
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Container } from 'react-bootstrap'
+import { ethers } from 'ethers'
+
+// components
+//import Navigation from './Navigation'
+//import Loading from './Loading'
+
+import {
+  loadProvider,
+  loadNetwork,
+  loadAccount
+} from '../store/interactions'
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  const loadBlockchainData = async () => {
+    // initiate provider
+    const provider = await loadProvider(dispatch)
+
+    // fetch chainId
+    const chainId = await loadNetwork(provider, dispatch)
+
+    // fetch accounts
+    await loadAccount(dispatch)
+
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App'
+import reportWebVitals from './reportWebVitals'
+
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
